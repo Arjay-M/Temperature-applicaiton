f280459f6a0c671f0947e586902ef109e7a172c7
Arjay-M <ACMacagba1@MY.SCCCD.EDU>
2025-11-26 11:15:11 -0800

    Update tempApplication146.m
    switched %.2f to .1f for time160F fprintf line

--- tempApplication146.m
+++ tempApplication146.m
@@ -20,10 +20,10 @@ fprintf("The room temp after 20 sesonds is %.2f°F\n",(T(L)))
 L = length(T);
 fprintf("The room temp after 40 sesonds is %.2f°F\n",(T(L)))
 
-%how long does it take tp get to 160f?
+%how long does it take to get to 160f?
 near160F = T>159.2 & T<160;
 time160F = t(near160F);
-fprintf("The time it takes to get to 160F %.2f seconds°F\n",time160F)
+fprintf("The time it takes to get to 160F %.1f seconds°F\n",time160F)
 
 %plot the function for the first 40 seconds
 %for the degrees symbol, the direct input cmd is ALT + 248
